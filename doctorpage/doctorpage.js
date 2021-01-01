542b715ca89b587a93dc6e45c5ef80d90262240c
Amirhoseinkh77 <ahkhanlarie@gmail.com>
2021-01-01 20:59:40 +0330

    Almost done

--- doctorpage/doctorpage.js
+++ doctorpage/doctorpage.js
@@ -1,7 +1,4 @@
 const id = localStorage.getItem("ID")
-
-console.log(id);
-
 const doctorName = document.querySelector('.secondPart h1')
 const text = document.querySelector('.text')
 const shomarenezampezeshki = document.querySelector('.secondPart h2')
@@ -14,6 +11,9 @@ const emtiazz = document.querySelector('.emtiaz .number')
 const allRate = document.querySelector('.nazarr')
 const commenter = document.querySelector('.person')
 const commentText = document.querySelector('.Comment span')
+const matabInformation = document.querySelector('.downside')
+const availableDays = document.querySelector('.availableDays')
+const matabButton = document.querySelector('.matabButton')
 const httpRequest = new XMLHttpRequest();
 httpRequest.open("GET",`https://intense-ravine-40625.herokuapp.com/doctors/${id}`)
 httpRequest.send();
@@ -29,11 +29,94 @@ httpRequest.onreadystatechange = function() {
         image.src=A.avatar
         takhasos1.textContent=A.spec
         name1.textContent=A.name
-        name1.textContent=A.name
         emtiazz.textContent = A.rate
         allRate.textContent = `از ${A.comments} نظر `
         commenter.textContent =  `نظر ${A.commenter}`
         commentText.textContent = A.comment_text
+        const tik = '<svg data-v-723181de="" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="green" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-dark-mint feather feather-check"><polyline data-v-723181de="" points="20 6 9 17 4 12"></polyline></svg>'
+        const cross = '<svg data-v-723181de="" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-primary feather feather-x"><line data-v-723181de="" x1="18" y1="6" x2="6" y2="18"></line><line data-v-723181de="" x1="6" y1="6" x2="18" y2="18"></line></svg>'
+        const checkDay = function (i) {
+            if (A.week_days[i]) {
+
+                return tik
+                
+                
+            } else{
+                return cross
+            }
+        }
+
+        const html1 =  `      <div class="top-side">
+        <div class="imageLocation">
+            <img src="158image.png" alt="">
+        </div>
+        <div class="textLocation">
+            <h4>
+    آدرس مطب ۱ :
+            </h4>
+            <span>
+            ${A.address}
+            </span>
+            <h4>
+                تلفن گویا : ${A.phone}
+            </h4>
+        </div>
+    </div>
+
+        `
+        const html2 = `
+        <p>مطب</p>
+        <div class="weekContainer">
+        <ul class="weekList">
+        <li>
+        شنبه
+        ${checkDay(0)}
+        </li>
+        <li>
+        یک شنبه
+        ${checkDay(1)}
+        </li>
+        <li>
+        دوشنبه
+        ${checkDay(2)}
+        </li>
+        <li>
+        سه شنبه
+        ${checkDay(3)}
+        </li>
+        <li>
+        چهار شنبه
+        ${checkDay(4)}
+        </li>
+        <li>
+        پنج شنبه
+        ${checkDay(5)}
+        </li>
+        <li>
+        جمعه
+        ${checkDay(6)}
+        </li>
+    </ul>
+        
+        
+        `
+        matabInformation.insertAdjacentHTML('afterbegin',html1)
+        matabButton.addEventListener('click',function () {
+            matabInformation.style.height = '20rem'
+            matabInformation.innerHTML = " "
+            matabInformation.insertAdjacentHTML('afterbegin',html1)
+            matabButton.style.backgroundColor = 'white'
+
+        })
+        availableDays.addEventListener('click',function () {
+
+            matabInformation.style.height = '15rem'
+            matabInformation.innerHTML = " "
+            matabInformation.insertAdjacentHTML('afterbegin',html2)
+
+
+        })
+       
 
 
     }
