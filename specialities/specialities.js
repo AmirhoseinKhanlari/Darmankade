96d525e133b0ff763bdf1d822991ed14a9a813c3
Amirhoseinkh77 <ahkhanlarie@gmail.com>
2021-01-05 16:49:32 +0330

    Slider Implemented

--- specialities/specialities.js
+++ specialities/specialities.js
@@ -1,16 +1,32 @@
-
-const moshahede = document.querySelectorAll('.specialitiesBox')
+const moshahede = document.querySelectorAll(".specialitiesBox");
 for (let index = 0; index < moshahede.length; index++) {
-    moshahede[index].addEventListener('click',function() {
-        window.location.href = '../doctorListPage/doctorListPage.html';
-    })
-    
+  moshahede[index].addEventListener("click", function () {
+    window.location.href = "../doctorListPage/doctorListPage.html";
+  });
 }
-const loginButton1 = document.querySelector('.loginButton')
-loginButton1.addEventListener('click',function() {
-    window.location.href = '../login.html';
-})
-const takhasos = document.querySelector('.specialList')
-takhasos.addEventListener('click',function() {
-    window.location.href = 'specialities.html';
-})
+const loginButton1 = document.querySelector(".loginButton");
+loginButton1.addEventListener("click", function () {
+  window.location.href = "../login.html";
+});
+const takhasos = document.querySelector(".specialList");
+takhasos.addEventListener("click", function () {
+  window.location.href = "specialities.html";
+});
+const slides = document.querySelectorAll(".slide");
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * (i-slide)}%)`;
+  });
+};
+goToSlide(0)
+let currSlide = 0;
+const maxSlide = slides.length
+setInterval(() => {
+    if(currSlide== maxSlide-1){
+        currSlide=0
+    }else {
+  currSlide++;
+    }
+  goToSlide(currSlide)
+  
+}, 4000);
